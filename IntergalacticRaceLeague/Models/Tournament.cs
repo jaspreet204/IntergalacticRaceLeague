6c262a4ec03fa12f9222659f53881cbfb47d9d7b
jaspreet <jasspreet4640@gmail.com>
2026-08-11 09:41:40 -0500

    Add Racer Vehicle and Tournament Entities

--- IntergalacticRaceLeague/Models/Tournament.cs
+++ IntergalacticRaceLeague/Models/Tournament.cs
@@ -2,5 +2,12 @@
 {
     public class Tournament
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Location { get; set; } = "";
+        public DateTime TournamentDate { get; set; }
+        public string Status { get; set; } = "";
+        public List<RacerTournament> RacerTournaments { get; set; }
+            = new List<RacerTournament>();
     }
 }
